1b1567c6583d0ca3a2b68d56f60c6b167e4f7239
Luis Palacios <luis.palacios.derqui@gmail.com>
2024-02-05 16:28:16 +0100

    desde el mac

--- 15-factorial/src/programa.cpp
+++ 15-factorial/src/programa.cpp
@@ -1,6 +1,6 @@
 // Programa para pruebas en C++
 //
-// Crear un progresión Aritmética y Geométrica dando 
+// Crear un progresión Aritmética y Geométrica dando
 // - número inicial
 // - la diferencia a aplicar en cada paso
 // - número de elementos a mostrar
@@ -15,20 +15,20 @@
 
 using namespace std;
 
-
-long factorial(long num) {
-    long ret=1;
-    if (num>0) ret = (num*factorial(num-1));
+long factorial(long num)
+{
+    long ret = 1;
+    if (num > 0)
+        ret = (num * factorial(num - 1));
     return ret;
 }
-
 
 // Entrada
 int main()
 {
     // Variables
-    long num=0; 
-    
+    long num = 0;
+
     // Configuro cout
     cout << setfill(' ');
     cout << setprecision(14);
@@ -37,10 +37,15 @@ int main()
     cout << "Número sobre el cual calcularé su factorial: ";
     cin >> num;
     cout << endl;
-
-    cout << "Factorial de " << num << "! = " << factorial(num) << endl;
-    cout << endl;
-
+    if (num < 0)
+    {
+        cout << "No puede ser negativo, la función factorial se define sobre el conjunto de los números naturales" << endl;
+    }
+    else
+    {
+        cout << "Factorial de " << num << "! = " << factorial(num) << endl;
+        cout << endl;
+    }
 
     return 0;
 }
